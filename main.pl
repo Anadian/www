72e37d3909a8b4729c8ca61159b11852c1aea2e8
Ubuntu <ubuntu@ip-172-31-46-192.us-west-2.compute.internal>
2016-08-26 03:27:01 +0000

    .

--- main.pl
+++ main.pl
@@ -1,9 +1,12 @@
 #!/usr/local/bin/perl
+use warnings;
+use strict;
 use CAX;
 CAX::NewLog();
 CAX::printl(0,"CAX test");
 use Dancer;
-
+set 'address' => 52.88.26.159;
+set 'port' => 80;
 get '/' => sub {
 	return '<html><head><title>Hello World!</title></head><body></body></html>';
 };
