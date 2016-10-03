a2d8fdf60f5a31660582bfd7ae87d851d97f17fb
Anadian <supercamman20@gmail.com>
2016-10-03 12:57:43 -0400

    Newer

--- main.pl
+++ main.pl
@@ -1,10 +1,11 @@
-#!/usr/bin/perl
-use warnings;
+#!/usr/local/bin/perl
 use strict;
+use warnings;
 use CAX;
 CAX::NewLog();
 CAX::printl(0,"CAX test");
 use Dancer;
+use Template;
 set 'port' => 80;
 get '/' => sub {
 	return '<html><head><title>Hello World!</title></head><body></body></html>';
